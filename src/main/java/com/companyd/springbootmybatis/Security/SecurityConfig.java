71cc8d1c08d17b40ab28dc17e1e609c7c6bf4a69
노설 <seven800@naver.com>
2020-06-01 19:44:04 +0900

    "20200601_회원가입정보전송확인_securityerror_PM19:38"

--- src/main/java/com/companyd/springbootmybatis/Security/SecurityConfig.java
+++ src/main/java/com/companyd/springbootmybatis/Security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.companyd.springbootmybatis.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,19 +13,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/mypage/**").authenticated() //로그인후 모든 엔드포인트
+                .antMatchers("/mypage/**").authenticated()
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                 .and()
                 .httpBasic()
                 .and()
-                .logout()
+                .logout() //logout configuration
                 .logoutUrl("/app-logout")
                 .deleteCookies("JSESSIONID")
                 .logoutSuccessUrl("/");
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
